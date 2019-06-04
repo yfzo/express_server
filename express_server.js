5c855b462523c50988c6bda6dce74abc3d7cff03
Yifei <yifeizhou.to@gmail.com>
2019-06-04 23:42:23 +0000

    Redirect to longURL if client makes get request for /u/:shortURL

--- express_server.js
+++ express_server.js
@@ -35,6 +35,12 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 })
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+
+  res.redirect(longURL);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
