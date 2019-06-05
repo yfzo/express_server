364418e21d9652ae3bdf42983f01a1f6226d10b1
Yifei <yifeizhou.to@gmail.com>
2019-06-05 19:54:02 +0000

    Can now display username in header

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ var PORT = 8080; // default port 8080
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -23,16 +23,27 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] /* What goes here? */ };
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 })
 
@@ -69,10 +80,14 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-
-  res.cookie('username', req.body.username);
+  res.cookie("username", req.body.username);
 
   res.redirect("/urls");
+})
+
+app.get('/logout', (req, res) => {
+  res.cookie('username', null);
+  res.redirect('/');
 })
 
 app.listen(PORT, () => {
