8671e968e75d7d154c8a48c61c26ba0b23c675bf
Yifei <yifeizhou.to@gmail.com>
2019-06-06 16:26:17 +0000

    Created registration page, a data store for users, and a registration handler, including errors.

--- express_server.js
+++ express_server.js
@@ -1,18 +1,20 @@
-var express = require("express");
-var crypto = require("crypto");
-var cookieParser = require("cookie-parser");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const crypto = require("crypto");
+const cookieParser = require("cookie-parser");
+const app = express();
+const PORT = 8080; // default port 8080
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.set("view engine", "ejs");
 
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const users = {};
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -54,6 +56,14 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.get("/register", (req, res) => {
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+
+  res.render("urls_register", templateVars);
+})
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
@@ -91,11 +101,42 @@ app.post('/logout', (req, res) => {
   res.redirect('/urls');
 })
 
+app.post('/register', (req, res) => {
+  let { email, password } = req.body;
+  let id = generateRandomString();
+
+  if (!email || !password) {
+    res.status(400).send('Missing email or password');
+  } else if (emailExists(email)) {
+    res.status(400).send('Email already used');
+  } else {
+    users[id] = {
+      id,
+      email,
+      password
+    };
+
+    res.cookie("user_id", id);
+    console.log(users);
+    res.redirect('/urls');
+  }
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
 function generateRandomString() {
   return crypto.randomBytes(3).toString('hex');
+}
+
+function emailExists(email) {
+  for (user in users) {
+    if (users[user].email == email) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
